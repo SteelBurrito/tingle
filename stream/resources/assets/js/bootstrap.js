e42c5e77b9e74182a949a143c17e0df525eafa33
SteelBurrito <antouniokevin@gmail.com>
2017-12-08 15:16:32 +0700

    Setting up environmet for development

--- stream/resources/assets/js/bootstrap.js
+++ stream/resources/assets/js/bootstrap.js
@@ -1,8 +1,17 @@
-import Vue from 'vue';
-import axios from 'axios';
 
-window.Vue = Vue;
-window.axios = axios;
+window._ = require('lodash');
+
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap-sass');
+} catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -27,3 +36,18 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+// import Echo from 'laravel-echo'
+
+// window.Pusher = require('pusher-js');
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: 'your-pusher-key'
+// });
